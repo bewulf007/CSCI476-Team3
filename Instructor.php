bc15382270ae651690d7772048a3e0672da2198c
WUmademedoit <morriss11@winthrop.edu>
2016-03-10 17:05:11 -0500

    Create Instructor.php

---
+++ Instructor.php
@@ -0,0 +1,20 @@
+<html>
+<hr>
+<table rules=all border=10>
+<tr>
+<td bgcolor=black colspan=8 align=center><font color=white>Existing Users
+<tr>
+<td bgcolor=green>id
+<td bgcolor=green>Street
+<td bgcolor=yellow>Update Street
+
+<?php
+// connect the database
+$DBconn = mysql_connect ("daytona.birdnest.org", "my.morriss11", "@y#mln52")
+          or exit ("failed to connect to mysql");
+$db_selected = mysql_select_db("my_morriss11", $DBconn);
+if (!$db_selected)
+   die ("Can't use my_paternom3 : " . mysql_error());
+   
+   if (isset($_POST['id']))
+  
