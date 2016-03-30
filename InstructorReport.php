b10822b3cb35c67568ab94be74f08fb1d80423fd
tjamison89 <jamisonm4@winthrop.edu>
2016-03-30 10:23:08 -0400

    Update InstructorReport.php

--- InstructorReport.php
+++ InstructorReport.php
@@ -21,6 +21,7 @@ $db_selected = mysql_select_db("my_morriss11", $DBconn);
 if (!$db_selected)
    die ("Can't use my_morriss11 : " . mysql_error());
 
+//get Instructor first name, last name, email, phone, building, and office for reporting purposes
 $query = "SELECT Instructor.Fname, Instructor.Lname, Instructor.Email, Instructor.Phone, Instructor.Building, Instructor.Office, Camp.Name FROM Instructor JOIN Camp WHERE Instructor.id=Camp.Instructor_id;
 ";
 $result = mysql_query ($query, $DBconn);
