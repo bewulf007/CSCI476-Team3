6143321cc5e528e48e79fd3942e1ad7c567b7524
WUmademedoit <morriss11@winthrop.edu>
2016-04-01 12:22:40 -0400

    Added files via upload

---
+++ file_readingcsv.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(0);
+if(!$file)
+echo 'Please enter a valid file name: ';
+
+//Requests a file name to send to readcsv.php
+echo "<form action=\"readingcsv.php\" method=\"post\">
+File Name: <input type=\"text\" name=\"file\"><br>
+<input type=\"submit\" value=\"Accept\">";
+
+if(isset($_POST['file']))
+{
+$file = $_POST['file'];
+
+echo "<br><br>You entered:<br>" . $file;
+}
+?>
+
+
