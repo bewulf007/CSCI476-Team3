cbc062daa10426fffcb13facb8b5bb8e1c702ae8
paternom3 <paternom3@winthrop.edu>
2016-04-01 14:03:09 -0400

    Update waitlist.php

--- waitlist.php
+++ waitlist.php
@@ -1,15 +1,19 @@
-<!-- Team 3 Summer Camp Project -->
+<!-- Matthew Paterno -->
 <!-- Database Program -->
-<!-- Allows Students to be Added to the Waitlist Table -->
+<!-- Allows Records to be Added to the Waitlist Table -->
+<!-- Contains one form that is also processed by this script -->
 
 <html>
 <hr>
 <table rules=all border=10>
 <tr>
-<td bgcolor=black colspan=8 align=center><font color=white>Existing Users
+<td bgcolor=black colspan=8 align=center><font color=white>Current Wait List
 <tr>
+<td bgcolor=green>Id
 <td bgcolor=green>Student_id
 <td bgcolor=yellow>Update Student_id
+<td bgcolor=green>First Name
+<td bgcolor=green>Last Name
 <td bgcolor=green>Camp_id
 <td bgcolor=yellow>Update Camp_id
 <td bgcolor=red>Delete
@@ -39,8 +43,7 @@ elseif (isset($_POST['remove']))
 	$query = "DELETE FROM Waitlist Student_id WHERE  = \"$remove\"";
 	$result = mysql_query ($query, $DBconn);
 }
-
-//change Student_id of Waitlist if update was clicked and if post is not empty
+//change Student_id of song if update was clicked and if post is not empty
 elseif (isset($_POST['UpdateT']))
 {
 	if(!empty($_POST['UpdatedStudent_id']))
@@ -56,8 +59,7 @@ elseif (isset($_POST['UpdateT']))
 }
 
 }
-
-//change Camp_id of Waitlist if update was clicked and if post is not empty
+//change Student_id of Camp_id if update was clicked and if post is not empty
 elseif (isset($_POST['UpdateD']))
 {
 	if(!empty($_POST['UpdatedCamp_id']))
@@ -74,8 +76,18 @@ elseif (isset($_POST['UpdateD']))
 
 }
 
-// submit and process the query for existing Waitlist
-$query = "select * from Waitlist;";
+/*elseif (isset($_POST['move']
+{
+	$move = $_POST['move'];
+        $query = "Insert INTO Campers
+	$query = "DELETE FROM Waitlist Student_id WHERE  = \"$move\"";
+	$result = mysql_query ($query, $DBconn);
+}*/
+// submit and process the query for existing Students
+$query = "Select Waitlist.id, Student_id, Fname, Lname, Waitlist.Camp_id
+ from Student JOIN Waitlist 
+WHERE Student.id=Waitlist.Student_id
+ORDER BY Student_id ASC;";
 $result = mysql_query ($query, $DBconn);
 while ($row = mysql_fetch_object ($result))
 {
@@ -86,6 +98,8 @@ while ($row = mysql_fetch_object ($result))
    echo ("<input type='hidden' name ='UpdateT' value = $row->Student_id>");  
    echo ("Change Student_id <input type=text name='UpdatedStudent_id'>");
    echo "</form>";
+   echo("<td> $row->Fname");
+   echo("<td> $row->Lname");
    echo ("<td> $row->Camp_id");
    echo ("<form action=waitlist.php method = post>");
    echo ("<td> <input type=submit value=Update>");
@@ -106,11 +120,16 @@ while ($row = mysql_fetch_object ($result))
 
 <form action=waitlist.php method=post>
 <pre>
-       New Waitlist:
+        New Waitlist:
 	Student_id <input type=text name="Student_id">
-	Camp_id <input type=text name="Camp_id">
+	Camp_id    <input type=text name="Camp_id">
        <input type=submit value="Add Record">
-<a href ="JoinedTable.php"> Songs And Artists Table </a>
+<P>
+	<a href ="Student.php">Student Table </a> - To Reference Student Id's
+<P>
+	<a href ="Camp.php">Camp Table </a> - To Reference Camp Id's
+<P>
+	<a href ="Administrator.html">Back To Administration </a>
 </pre>
 </form>
 <P>
