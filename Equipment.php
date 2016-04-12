628e84d0a38fc9d7efc8c6e98c654f11dffa7b71
WUmademedoit <morriss11@winthrop.edu>
2016-04-12 18:16:20 -0400

    Update Equipment.php

--- Equipment.php
+++ Equipment.php
@@ -1,3 +1,6 @@
+<!-- Summer Camp Project -->
+<!-- Database Program -->
+<!-- Allows Equipment to be Added to the Equipment Table -->
 <html>
 <hr>
 <table rules=all border=10>
@@ -15,36 +18,36 @@
 
 <?php
 // connect the database
-$DBconn = mysql_connect ("daytona.birdnest.org", "my.morriss11", "@y#mln52")
+$DBconn = mysqli_connect ("daytona.birdnest.org", "my.morriss11", "@y#mln52")
           or exit ("failed to connect to mysql");
-$db_selected = mysql_select_db("my_morriss11", $DBconn);
+$db_selected = mysqli_select_db($DBconn, "my_morriss11");
 if (!$db_selected)
-   die ("Can't use my_paternom3 : " . mysql_error());
+   die ("Can't use my_paternom3 : " . mysqli_error());
    
    //If the form has data, then insert a new record
    if (isset($_POST['Name']))
   {
-   $Name= $_POST['Name'];
+   $Name= mysqli_real_escape_string($DBconn, trim($_POST['Name']));
    //$Student_id = $_POST['Student_id'];
-   $Serial_number = $_POST['Serial_number'];
+   $Serial_number = mysqli_real_escape_string($DBconn, trim($_POST['Serial_number']));
    $query = "INSERT INTO Equipment VALUES (NULL,'$Name','$Serial_number')";
-   $result = mysql_query ($query, $DBconn);
+   $result = mysqli_query ($DBconn, $query);
   }
 elseif (isset($_POST['remove']))
 {
-	$remove = $_POST['remove'];
+	$remove = mysqli_real_escape_string($DBconn, trim($_POST['remove']));
 	$query = "DELETE FROM Equipment WHERE id = \"$remove\"";
-	$result = mysql_query ($query, $DBconn);
+	$result = mysqli_query ($DBconn, $query);
 }
 //change Name of Equipment if update was clicked and if post is not empty
 elseif (isset($_POST['UpdateN']))
 {
 	if(!empty($_POST['UpdatedName']))
 {
-	$update = $_POST['UpdateN'];
-	$UpdateName= $_POST['UpdatedName'];
+	$update = mysqli_real_escape_string($DBconn, trim($_POST['UpdateN']));
+	$UpdateName= mysqli_real_escape_string($DBconn, trim($_POST['UpdatedName']));
 	$query = "UPDATE Equipment SET Name = \"$UpdateName\" WHERE id = $update;";
-        $result = mysql_query($query, $DBconn);
+        $result = mysqli_query($DBconn, $query);
 }
 	if(empty($_POST['UpdatedName']))
 {
@@ -60,34 +63,32 @@ elseif (isset($_POST['UpdateSID']))
 	$update = $_POST['UpdateSID'];
 	$UpdateStudent_id= $_POST['UpdatedStudent_id'];
 	$query = "UPDATE Equipment SET Student_id = \"$UpdateStudent_id\" WHERE id = $update;";
-        $result = mysql_query($query, $DBconn);
+        $result = mysqli_query($query, $DBconn);
 }
 	if(empty($_POST['UpdatedStudent_id']))
 {
 	throw new InvalidArgumentException('Invalid Student_id');
 }
 }*/
-
 //change Serial_number of Equipment if update was clicked and if post is not empty
 elseif (isset($_POST['UpdateSN']))
 {
 	if(!empty($_POST['UpdatedSerial_number']))
 {
-	$update = $_POST['UpdateSN'];
-	$UpdateSerial_number= $_POST['UpdatedSerial_number'];
+	$update = mysqli_real_escape_string($DBconn, trim($_POST['UpdateSN']));
+	$UpdateSerial_number= mysqli_real_escape_string($DBconn, trim($_POST['UpdatedSerial_number']));
 	$query = "UPDATE Equipment SET Serial_number = \"$UpdateSerial_number\" WHERE id = $update;";
-        $result = mysql_query($query, $DBconn);
+        $result = mysqli_query($DBconn, $query);
 }
 	if(empty($_POST['UpdatedSerial_number']))
 {
 	throw new InvalidArgumentException('Invalid Serial_number');
 }
 }
-
 // submit and process the query for exisiting Equipments
 $query = "select * from Equipment;";
-$result = mysql_query ($query, $DBconn);
-while ($row = mysql_fetch_object ($result))
+$result = mysqli_query ($DBconn, $query);
+while ($row = mysqli_fetch_object ($result))
 {
    echo ("<tr> <td> $row->id");
    echo("<td> $row->Name");
@@ -124,8 +125,8 @@ while ($row = mysql_fetch_object ($result))
 <pre>
 		New Equipment Info:
 		Name <input type=text name="Name">
-		<!--Student_id <input type=text name="Student_id">-->
 		Serial_number <input type=text name="Serial_number">
+		<!--Student_id <input type=text name="Student_id">-->
        <input type=submit value="Add Record">
 <a href ="JoinedTable.php"> Addresses And Zips Table </a>
 </pre>
