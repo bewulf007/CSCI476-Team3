2863956b29a3ae1d4fdbd27551e6101226a89fef
WUmademedoit <morriss11@winthrop.edu>
2016-04-12 18:13:45 -0400

    Update Camp.php

--- Camp.php
+++ Camp.php
@@ -1,3 +1,6 @@
+<!-- Summer Camp Project -->
+<!-- Database Program -->
+<!-- Allows Budgets to be Added to the Budgets Table -->
 <html>
 <hr>
 <table rules=all border=10>
@@ -17,79 +20,76 @@
 
 <?php
 // connect the database
-$DBconn = mysql_connect ("daytona.birdnest.org", "my.morriss11", "@y#mln52")
+$DBconn = mysqli_connect ("daytona.birdnest.org", "my.morriss11", "@y#mln52")
           or exit ("failed to connect to mysql");
-$db_selected = mysql_select_db("my_morriss11", $DBconn);
+$db_selected = mysqli_select_db($DBconn, "my_morriss11");
 if (!$db_selected)
-   die ("Can't use my_paternom3 : " . mysql_error());
+   die ("Can't use my_morriss11 : " . mysqli_error());
+
 // if the form has data, then insert a new record
 if (isset($_POST['Name']))
   {
-   $Name= $_POST['Name'];
-   $MaxNum= $_POST['MaxNum'];
-   $Instructor_id = $_POST['Instructor_id'];
+   $Name= mysqli_real_escape_string($DBconn, trim($_POST['Name']));
+   $MaxNum= mysqli_real_escape_string($DBconn, trim($_POST['MaxNum']));
+   $Instructor_id = mysqli_real_escape_string($DBconn, trim($_POST['Instructor_id']));
    $query = "INSERT INTO Camp VALUES (NULL,'$Name','$MaxNum','$Instructor_id')";
-   $result = mysql_query ($query, $DBconn);
+   $result = mysqli_query ($DBconn, $query) or die ('Error querying database.');
   }
 elseif (isset($_POST['remove']))
 {
-	$remove = $_POST['remove'];
+	$remove = mysqli_real_escape_string($DBconn, trim($_POST['remove']));
 	$query = "DELETE FROM Camp WHERE id = \"$remove\"";
-	$result = mysql_query ($query, $DBconn);
+	$result = mysqli_query ($DBconn, $query) or die ('Error querying database.');
 }
-
 //change Name of Camp if update was clicked and if post is not empty
 elseif (isset($_POST['UpdateN']))
 {
 	if(!empty($_POST['UpdatedName']))
 {
-	$update = $_POST['UpdateN'];
-	$UpdateName= $_POST['UpdatedName'];
+	$update = mysqli_real_escape_string($DBconn, trim($_POST['UpdateN']));
+	$UpdateName= mysqli_real_escape_string($DBconn, trim($_POST['UpdatedName']));
 	$query = "UPDATE Camp SET Name = \"$UpdateName\" WHERE id = $update;";
-        $result = mysql_query($query, $DBconn);
+    $result = mysqli_query ($DBconn, $query) or die ('Error querying database.');
 }
 	if(empty($_POST['UpdatedName']))
 {
 	throw new InvalidArgumentException('Invalid Name');
 }
 }
-
 //change MaxNum of Camp if update was clicked and if post is not empty
 elseif (isset($_POST['UpdateMN']))
 {
 	if(!empty($_POST['UpdatedMaxNum']))
 {
-	$update = $_POST['UpdateMN'];
-	$UpdateMaxNum= $_POST['UpdatedMaxNum'];
+	$update = mysqli_real_escape_string($DBconn, trim($_POST['UpdateMN']));
+	$UpdateMaxNum= mysqli_real_escape_string($DBconn, trim($_POST['UpdatedMaxNum']));
 	$query = "UPDATE Camp SET MaxNum = \"$UpdateMaxNum\" WHERE id = $update;";
-        $result = mysql_query($query, $DBconn);
+    $result = mysqli_query ($DBconn, $query) or die ('Error querying database.');
 }
 	if(empty($_POST['UpdatedMaxNum']))
 {
 	throw new InvalidArgumentException('Invalid MaxNum');
 }
 }
-
 //change Instructor of Camp if update was clicked and if post is not empty
 elseif (isset($_POST['UpdateI']))
 {
 	if(!empty($_POST['UpdatedInstructor_id']))
 {
-	$update = $_POST['UpdateI'];
-	$UpdateInstructor_id= $_POST['UpdatedInstructor_id'];
+	$update = mysqli_real_escape_string($DBconn, trim($_POST['UpdateI']));
+	$UpdateInstructor_id= mysqli_real_escape_string($DBconn, trim($_POST['UpdatedInstructor_id']));
 	$query = "UPDATE Camp SET Instructor_id = \"$UpdateInstructor_id\" WHERE id = $update;";
-        $result = mysql_query($query, $DBconn);
+    $result = mysqli_query ($DBconn, $query) or die ('Error querying database.');
 }
 	if(empty($_POST['UpdatedInstructor_id']))
 {
 	throw new InvalidArgumentException('Invalid Instructor_id');
 }
 }
-
 // submit and process the query for existing Instructors
 $query = "select * from Camp;";
-$result = mysql_query ($query, $DBconn);
-while ($row = mysql_fetch_object ($result))
+$result = mysqli_query ($DBconn, $query) or die ('Error querying database.');
+while ($row = mysqli_fetch_object ($result))
 {
    echo ("<tr> <td> $row->id");
    echo("<td> $row->Name");
@@ -125,11 +125,11 @@ while ($row = mysql_fetch_object ($result))
 <form action=Camp.php method=post>
 <pre>
        New Camp Info:
-        Name <input type=text name="Name">
-	      MaxNum <input type=text name="MaxNum">
-	      Instructor_id <input type=text name="Instructor_id">
+       Name <input type=text name="Name">
+	   MaxNum <input type=text name="MaxNum">
+	   Instructor_id <input type=text name="Instructor_id">
        <input type=submit value="Add Camp Info">
-<a href ="JoinedTable.php"> Camp Contact Table </a>
+<a href ="Administrator.html"> Back To Administration </a>
 </pre>
 </form>
 <P>
