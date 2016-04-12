9220b2ffde99cf0968642a5bde7df12dde18ef75
WUmademedoit <morriss11@winthrop.edu>
2016-04-12 18:17:02 -0400

    Update EquipmentList.php

--- EquipmentList.php
+++ EquipmentList.php
@@ -1,3 +1,6 @@
+<!-- Summer Camp Project -->
+<!-- Database Program -->
+<!-- Allows Budgets to be Added to the Budgets Table -->
 <html>
 <hr>
 <table rules=all border=10>
@@ -8,21 +11,20 @@
 
 <?php
 // connect the database
-$DBconn = mysql_connect ("daytona.birdnest.org", "my.morriss11", "@y#mln52")
+$DBconn = mysqli_connect ("daytona.birdnest.org", "my.morriss11", "@y#mln52")
           or exit ("failed to connect to mysql");
-$db_selected = mysql_select_db("my_morriss11", $DBconn);
+$db_selected = mysqli_select_db($DBconn, "my_morriss11");
 if (!$db_selected)
-   die ("Can't use my_paternom3 : " . mysql_error());
+   die ("Can't use my_paternom3 : " . mysqli_error());
 
 $query = "select * from Equipment;";
-$result = mysql_query ($query, $DBconn);
-while ($row = mysql_fetch_object ($result))
+$result = mysqli_query ($DBconn, $query);
+while ($row = mysqli_fetch_object ($result))
 {
    echo ("<tr> <td> $row->Name");
 }
 
 echo ("</table>");
 echo ("<button onclick=history.go(-1);>Back </button>");
-
 
 ?>
