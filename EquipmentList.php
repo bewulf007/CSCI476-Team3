44e6540330efde90e50bddfe5bf138f9905a332f
WUmademedoit <morriss11@winthrop.edu>
2016-04-18 10:10:24 -0400

    Update EquipmentList.php

--- EquipmentList.php
+++ EquipmentList.php
@@ -3,33 +3,29 @@
 <!-- Displays a list of Equipment -->
 <html>
 <hr>
-<!-- output table for crud -->
 <table rules=all border=10>
 <tr>
-<td bgcolor=White colspan=8 align=center><font color=Black>Equipment Report
+<td bgcolor=White colspan=2 align=center><font color=Black>Equipment Report
 <tr>
 <td bgcolor=White>Name
+<td bgcolor=White>Serial Number
 
-<!--end of table -->
-
-<?php //php begins here
+<?php
 // connect the database
 $DBconn = mysqli_connect ("daytona.birdnest.org", "my.morriss11", "@y#mln52")
           or exit ("failed to connect to mysql");
 $db_selected = mysqli_select_db($DBconn, "my_morriss11");
-//exception if database cannot connect throw error message
 if (!$db_selected)
-   die ("Can't use my_morriss11 : " . mysqli_error());
-//query to create report
+   die ("Can't use my_paternom3 : " . mysqli_error());
+
 $query = "select * from Equipment;";
 $result = mysqli_query ($DBconn, $query);
 while ($row = mysqli_fetch_object ($result))
 {
-//generate query into the table
    echo ("<tr> <td> $row->Name");
+   echo ("<td> $row->Serial_number");
 }
 
 echo ("</table>");
 echo ("<button onclick=history.go(-1);>Back </button>");
-
 ?>
