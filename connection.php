b5485f42f36abceba3726cf0d4cd2656c1a409dc
WUmademedoit <morriss11@winthrop.edu>
2016-04-18 16:51:40 -0400

    Added files via upload

---
+++ connection.php
@@ -0,0 +1,12 @@
+<!-- Summer Camp Project -->
+<!-- Database Program -->
+<!-- Allows Addresses to be Added to the Address Table -->
+
+<?php
+// connect the database
+$DBconn = mysqli_connect ("daytona.birdnest.org", "my.morriss11", "@y#mln52")
+          or exit ("failed to connect to mysql");
+$db_selected = mysqli_select_db($DBconn, "my_morriss11");
+if (!$db_selected)
+   die ("Can't use my_morriss11 : " . mysqli_error());
+?>
